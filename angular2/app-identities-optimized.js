30c6404b332d3df6a6be520222788e1ce7845c4b
notbakaneko <bakaneko@pixiv.com>
2017-01-18 19:55:40 +0900

    upgrade Angular2 to 2.4.1

--- angular2/app-identities-optimized.js
+++ angular2/app-identities-optimized.js
@@ -1,24 +1,33 @@
-var AppComponent = ng.
-  Component({
-    selector: 'my-app'
-  }).
-  View({
-    directives: [ng.NgFor],
-    templateUrl: 'app-component.html'
-  }).
-  Class({
-    constructor: function AppComponent() {
-      var me = this;
-      this.databases = [];
-      var load = function() {
-          me.databases = ENV.generateData(true).toArray();
-          Monitoring.renderRate.ping();
-          setTimeout(load, ENV.timeout);
-      };
-      load();
-    }
-  });
+var AppComponent = function() {
+    var me = this;
+    this.databases = [];
+    var load = function() {
+        me.databases = ENV.generateData(true).toArray();
+        Monitoring.renderRate.ping();
+        setTimeout(load, ENV.timeout);
+    };
+    load();
+};
+
+AppComponent.annotations = [
+  new ng.core.Component({
+    selector : 'my-app',
+    templateUrl : 'app-component.html'
+  })
+];
+
+
+var AppModule = function AppModule() {}
+
+AppModule.annotations = [
+  new ng.core.NgModule({
+    imports: [ng.platformBrowser.BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+  })
+]
 
 document.addEventListener('DOMContentLoaded', function() {
-  ng.bootstrap(AppComponent);
+  ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(
+      AppModule);
 });
